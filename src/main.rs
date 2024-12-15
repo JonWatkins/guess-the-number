65db62a93aeeba3bd1ef6620b49f5c4ae360b0d8
Jon Watkins <jonwatkins86@gmail.com>
2024-12-15 19:39:21 +0000

    Restructured the code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use guessing_game::{ErrorHandler, Guess, GuessCount, GuessResult, Incrementable};
+use guessing_game::{
+    get_guess, get_secret_number, handle_guess, ErrorHandler, Guess, GuessCount, GuessResult,
+    Incrementable,
+};
 
 /// The main function that runs the game loop for guessing the secret number.
 ///
@@ -21,8 +24,8 @@ fn main() {
     println!("Guess the number");
 
     // Generate a random secret number between 1 and 100.
-    let secret_number = Guess::new(guessing_game::get_secret_number(1, 100))
-        .expect("Failed to generate secret number");
+    let secret_number =
+        Guess::new(get_secret_number(1, 100)).expect("Failed to generate secret number");
 
     // Initialize the guess count to track the number of attempts.
     let mut guess_count = GuessCount::new();
@@ -30,7 +33,7 @@ fn main() {
     // Game loop: continue until the user guesses correctly.
     loop {
         // Get the user's guess and handle any errors (invalid input or parsing errors).
-        let guess = match guessing_game::get_guess() {
+        let guess = match get_guess() {
             Ok(g) => g,
             Err(err) => {
                 // Handle input error (e.g., out of range or invalid input).
@@ -43,7 +46,7 @@ fn main() {
         guess_count.increment();
 
         // Compare the guess to the secret number and check if the user wins.
-        match guessing_game::handle_guess(guess, &secret_number) {
+        match handle_guess(guess, &secret_number) {
             GuessResult::TooSmall => println!("Too small"),
             GuessResult::TooBig => println!("Too big"),
             GuessResult::Correct => {
